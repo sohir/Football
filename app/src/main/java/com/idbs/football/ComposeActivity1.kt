e5671b6e0c62760d089514df5cee8b31d9955813
sohir <sohirhashem@gmail.com>
2022-07-05 13:00:34 +0200

    basis layouts

--- app/src/main/java/com/idbs/football/ComposeActivity1.kt
+++ app/src/main/java/com/idbs/football/ComposeActivity1.kt
@@ -1,27 +1,36 @@
 package com.idbs.football
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.idbs.football.ui.theme.FootballTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class ComposeActivity1 : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,20 +43,38 @@ class ComposeActivity1 : ComponentActivity() {
                 }
             }
             //Column like linearLayout
-            Column (modifier = Modifier.fillMaxSize(),
+/*            Column (modifier = Modifier.fillMaxSize(),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Center
             ){
-                MyText(myText.value)
-                MyTextField()
-                MyTOutLineTextField()
-                MyBtn()
-                MyRadioBtn()
-                MyFloatBtn()
-                MyProgress()
+            //    MyText(myText.value)
+            //    MyTextField()
+            //    MyTOutLineTextField()
+            //    MyBtn()
+            //    MyRadioBtn()
+            //    MyFloatBtn()
+            //    MyProgress()
+            //    MyAlertDialog()
+                Row(
+                    modifier = Modifier
+                        .background(color = colorResource(id = R.color.teal_200))
+                        .fillMaxSize()
+                        .width(300.dp)
+                        .height(300.dp),
+                    verticalAlignment = Alignment.Top,
+                    horizontalArrangement = Arrangement.SpaceEvenly,
+
+                ) {
+                    MyRow()
+                }
+                }*/
+       /*     Box(modifier = Modifier.fillMaxSize()) {
+                MySurface(modifier = Modifier.align(Alignment.Center))
+            }*/
+            MyScaffold()
             }
         }
-    }
+
 }
 val myText = mutableStateOf("")
 
@@ -146,6 +173,123 @@ fun MyProgress(){
     CircularProgressIndicator(
         color = colorResource(id = R.color.teal_700),
         strokeWidth = 5.dp,
+    )
+}
+@Composable
+fun MyAlertDialog(){
+    val shouldShowDialog = remember {
+        mutableStateOf(true)
+    }
+    if (shouldShowDialog.value){
+
+        AlertDialog(
+        onDismissRequest =
+        {shouldShowDialog.value = false },
+        title = {Text("Title Alert Dialog")},
+        text ={ Text(text = "Text Alert Dialog") },
+            confirmButton = {
+                Button(
+                    onClick =
+                    {shouldShowDialog.value = false
+                    }) {
+                    Text(text = "Confirm", color = colorResource(id = R.color.teal_700))
+                }
+            },
+            dismissButton = {
+                Button(
+                    onClick =
+                    {shouldShowDialog.value = false
+                    }) {
+                    Text(text = "Cancel", color = colorResource(id = R.color.teal_700))
+                }
+            }
+        )
+    }
+}
+
+@Composable
+fun RowScope.MyRow(){
+    Text(text = "One", modifier = Modifier.weight(1f), style = TextStyle(background = Color.Yellow))
+    Text(text = "Two", modifier = Modifier.weight(1f), style = TextStyle(background = Color.Yellow))
+    Text(text = "Three", modifier = Modifier.weight(1f), style = TextStyle(background = Color.Yellow))
+
+}
+@Composable
+fun MyColumn(
+
+){
+    val context = LocalContext.current
+
+    Column(modifier = Modifier.fillMaxSize(),
+    verticalArrangement = Arrangement.SpaceEvenly,
+    horizontalAlignment = Alignment.Start) {
+        Button(onClick = {
+           context.startActivity(Intent(context, ListComposeActivity::class.java))
+
+        }) {
+            Text(text = "Click", color = colorResource(id = R.color.teal_700))
+
+        }
+        Text(text = "One", fontSize = 22.sp, style = TextStyle(background = Color.Yellow),)
+        Text(text = "Two", fontSize = 22.sp, style = TextStyle(background = Color.Yellow))
+        Text(text = "Three", fontSize = 22.sp, style = TextStyle(background = Color.Yellow))
+
+    }
+}
+fun navigation(){
+
+}
+@Composable
+fun MySurface(modifier: Modifier){
+    Surface(modifier = Modifier.size(200.dp),
+        color = Color.LightGray, contentColor = colorResource(
+        id = R.color.purple_200),
+        border = BorderStroke(4.dp, color = Color.Yellow))
+    {
+        MyColumn()
+    }
+}
+
+@Composable
+fun MyScaffold(){
+    val scaffoldState:ScaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+    Scaffold (content = { MyColumn()},
+    topBar = {MyToolbar(scaffoldState,scope)},
+        drawerContent = {
+            MyColumn()
+        }
+    , bottomBar = { MyBottomAppBar()}
+    )
+}
+@Composable
+fun MyToolbar(scaffoldState: ScaffoldState,scope:CoroutineScope){
+    val drawerState = scaffoldState.drawerState
+
+    TopAppBar (
+        navigationIcon = {
+            IconButton(onClick = {
+                                 scope.launch(Dispatchers.Main) {
+                                     if (drawerState.isClosed)
+                                         drawerState.open() else drawerState.close()
+                                 }
+                                 },
+                content = {
+                    Icon(Icons.Default.Menu, contentDescription = "this is a toolbar")
+                }
+            )
+        },
+        title = { Text(text = stringResource(id = R.string.app_name))},
+        backgroundColor = Color.Red
+    )
+}
+
+
+@Composable
+fun MyBottomAppBar(){
+    BottomAppBar(
+        content = {},
+        backgroundColor = Color.Green
     )
 }
 
