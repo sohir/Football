f80a68dd2757c72635a7f6eb6c30fd92be3f6fa8
sohir <sohirhashem@gmail.com>
2022-07-24 23:30:24 +0200

    Custom Circular ProgressBar

--- app/src/main/java/com/idbs/football/activites/philipp_lackner/AnimmationssAtivity.kt
+++ app/src/main/java/com/idbs/football/activites/philipp_lackner/AnimmationssAtivity.kt
@@ -5,8 +5,10 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.animateColor
 import androidx.compose.animation.core.*
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
@@ -16,8 +18,15 @@ import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
 import com.idbs.football.activites.philipp_lackner.ui.theme.FootballTheme
 
 class AnimmationssAtivity : ComponentActivity() {
@@ -27,7 +36,10 @@ class AnimmationssAtivity : ComponentActivity() {
             FootballTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(color = MaterialTheme.colors.background) {
-                    SimpleAnimation()
+                  //  SimpleAnimation()
+                    Box(contentAlignment = Alignment.Center,modifier = Modifier.fillMaxSize()){
+                        CircularProgressBar(percentage = 0.8f, number = 100 )
+                    }
                 }
             }
         }
@@ -75,6 +87,47 @@ fun SimpleAnimation() {
         Text(text = "Increase size")
     }
 }
+}
+
+@Composable
+fun CircularProgressBar(
+    percentage:Float,
+    number:Int,
+    fontSize:TextUnit = 28.sp,
+    radius: Dp = 50.dp,
+    color:Color = Color.Green,
+    strokeWidth:Dp = 8.dp,
+    animDuration: Int = 1000,
+    animDelay: Int = 0
+){
+    var animationPlayed by remember {
+        mutableStateOf(false)
+    }
+    val curPercentage = animateFloatAsState(
+        targetValue = if (animationPlayed) percentage else 0f,
+        animationSpec = tween(
+            durationMillis = animDuration,
+            delayMillis = animDelay
+        )
+    )
+    LaunchedEffect(key1 = true ){ animationPlayed = true }
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier.size(radius * 2f) ){
+        Canvas(modifier = Modifier.size(radius * 2f)){
+            drawArc(color = color,
+            -90f,
+            360 * curPercentage.value,
+            useCenter = false,
+            style = Stroke(strokeWidth.toPx(), cap = StrokeCap.Round)
+            )
+        }
+        Text(text = (curPercentage.value * number).toInt().toString(),
+        color = Color.Black, fontSize = fontSize,
+        fontWeight = FontWeight.Bold
+        )
+
+    }
 }
 
 @Preview(showBackground = true)
